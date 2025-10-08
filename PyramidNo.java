ac598c6b9e8d2d801174078dc98c4ac175fca9aa
jayesh-exe <jsyeshsharma@gmail.com>
2025-10-08 18:09:14 +0530

    second commit

---
+++ PyramidNo.java
@@ -0,0 +1,21 @@
+class PyramidNo
+{
+    public static void main(String args[])
+    { int a=1;
+        for(int r=1;r<=5;++r)
+        {
+            for(int c=1;c<=9;++c)
+            {
+                if()
+                {
+                    System.out.print("*");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
